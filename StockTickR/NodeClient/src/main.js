1118f889fed0819bc9ce99df6d375ac4c4abc3af
Daniele Giove <Daniele.Giove@pioneerinvestments.com>
2018-03-23 11:05:45 +0100

    dg

--- StockTickR/NodeClient/src/main.js
+++ StockTickR/NodeClient/src/main.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const signalR = require("@aspnet/signalr-client");
+const signalR = require("@aspnet/signalr");
 const EventSource = require("eventsource");
 global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-global.WebSocket = require("websocket").w3cwebsocket;
+global.WebSocket = require("websocket").client;
 global.EventSource = EventSource;
 global.btoa = require("btoa");
 global.atob = require("atob");
-const hubConnection = new signalR.HubConnection("http://localhost:5000/stocks");
+const hubConnection = new signalR.HubConnection("http://localhost:5000/stocks", { transport: signalR.TransportType.WebSockets });
 hubConnection.start().then(() => {
     hubConnection.invoke("GetMarketState").then(function (state) {
         if (state === "Open") {
@@ -17,19 +17,12 @@ hubConnection.start().then(() => {
             hubConnection.invoke("OpenMarket");
         }
     });
-}).catch(err => {
-    console.log(err);
-});
-hubConnection.on("marketOpened", function () {
-    streamStocks();
-});
-function streamStocks() {
+}).catch(process.stderr.write);
+hubConnection.on("marketOpened", () => streamStocks());
+const streamStocks = () => {
     hubConnection.stream("StreamStocks").subscribe({
-        next: (stock) => {
-            console.log(stock);
-            // console.log(stock.Symbol + " " + stock.Price);
-        },
+        next: process.stdout.write,
         error: () => { },
         complete: () => { }
     });
-}
+};
--- StockTickR/NodeClient/src/main.ts
+++ StockTickR/NodeClient/src/main.ts
@@ -1,37 +1,30 @@
-import * as signalR from "@aspnet/signalr-client";
+import * as signalR from "@aspnet/signalr";
 import * as EventSource from "eventsource";
 
 (<any>global).XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-(<any>global).WebSocket = require("websocket").w3cwebsocket;
+(<any>global).WebSocket = require("websocket").client;
 (<any>global).EventSource = EventSource;
 (<any>global).btoa = require("btoa");
 (<any>global).atob = require("atob");
 
-const hubConnection : signalR.HubConnection = new signalR.HubConnection("http://localhost:5000/stocks");
+const hubConnection: signalR.HubConnection = new signalR.HubConnection("http://localhost:5000/stocks", { transport: signalR.TransportType.WebSockets });
 
 hubConnection.start().then(() => {
-    hubConnection.invoke("GetMarketState").then(function (state : string): void {
+    hubConnection.invoke("GetMarketState").then(function (state: string): void {
         if (state === "Open") {
             streamStocks();
         } else {
             hubConnection.invoke("OpenMarket");
         }
     });
-}).catch(err => {
-    console.log(err);
-});
+}).catch(process.stderr.write);
 
-hubConnection.on("marketOpened", function(): void {
-    streamStocks();
-});
+hubConnection.on("marketOpened", () => streamStocks());
 
-function streamStocks (): void {
+const streamStocks = () => {
     hubConnection.stream("StreamStocks").subscribe({
-        next: (stock : any) => {
-            console.log(stock);
-            // console.log(stock.Symbol + " " + stock.Price);
-        },
-        error: () => {},
-        complete: () => {}
+        next: process.stdout.write,
+        error: () => { },
+        complete: () => { }
     });
 }
